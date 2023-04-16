dfcae3c3af2ff88ab10b33b8dd0cff6470c65695
Abhinav <Abhinav.Deshpande@iiit.ac.in>
2023-04-16 23:57:54 +0530

    Submission

---
+++ code/game_data.py
@@ -0,0 +1,22 @@
+level_0 = {
+		'node_pos':(110,400),
+        'unlock':1,
+		'terrain': '../levels/0/level_0_terrain.csv',
+		'crates': '../levels/0/level_0_crates.csv',
+		'enemies':'../levels/0/level_0_enemies.csv',
+		'constraints':'../levels/0/level_0_constraints.csv',
+		'player': '../levels/0/level_0_player.csv',
+        'background':'../graphics/background/background.png'
+		}
+
+level_1={
+    'node_pos':(300,220),
+    'unlock':2,
+    'player': '../levels/0/level_0_player.csv',
+    'background':'../graphics/background/lab.jpg'
+}
+
+levels={
+    0:level_0,
+    1:level_1,
+}
